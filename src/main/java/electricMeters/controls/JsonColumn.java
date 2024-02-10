40743cc6c8f1b4c01ecb5f7fb1d5d1b28653ecc4
isachenkoff <isachenkoff.alexander@yandex.ru>
2024-02-10 23:30:44 +0800

    API для выравнивания столбцов

--- src/main/java/electricMeters/controls/JsonColumn.java
+++ src/main/java/electricMeters/controls/JsonColumn.java
@@ -1,6 +1,7 @@
 package electricMeters.controls;
 
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.geometry.Pos;
 import javafx.scene.control.TableColumn;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,8 @@ import org.json.JSONObject;
 public class JsonColumn extends TableColumn<JSONObject, Object> {
     
     private String field;
+    @Getter
+    private Pos alignment;
     
     public JsonColumn() {
         this.setCellValueFactory(param -> {
@@ -18,6 +21,20 @@ public class JsonColumn extends TableColumn<JSONObject, Object> {
             Object value = jsonObject.has(field) ? jsonObject.get(field) : "";
             return new SimpleObjectProperty<>(value);
         });
+        styleProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.endsWith(getAlignmentStyleString(this.alignment))) {
+                setAlignment(this.alignment);
+            }
+        });
+    }
+    
+    public void setAlignment(Pos alignment) {
+        this.alignment = alignment;
+        setStyle(getStyle() + getAlignmentStyleString(alignment));
+    }
+    
+    private static String getAlignmentStyleString(Pos alignment) {
+        return "-fx-alignment: " + alignment + ";";
     }
     
 }
