1020e1edc6b7e6e0dba2b1ed5203dc49e829a0da
isachenkoff <isachenkoff.alexander@yandex.ru>
2024-02-17 15:08:17 +0800

    оптимизировал запрос к HOURLY_RATE_VALUES

--- src/main/resources/electricMeters/sql/HOURLY_RATE_VALUES.sql
+++ src/main/resources/electricMeters/sql/HOURLY_RATE_VALUES.sql
@@ -1,6 +1,5 @@
-select val.* from HOURLY_RATE_VALUES val
-where (
-    select VOLTAGE_LEVEL_ID from HOURLY_POWER_RATE hpr
-    where hpr.ID = val.HOURLY_POWER_RATE_ID
-    and (select ID from POWER_RATE where YEAR = ? and MONTH = ? and POWER_RATE_TYPE_ID = ?) = hpr.POWER_RATE_ID
-) = ?
+select * from HOURLY_RATE_VALUES
+where HOURLY_POWER_RATE_ID =
+      (select ID from HOURLY_POWER_RATE
+       where POWER_RATE_ID = (select ID from POWER_RATE where YEAR = ? and MONTH = ? and POWER_RATE_TYPE_ID = ?)
+         and VOLTAGE_LEVEL_ID = ?)
