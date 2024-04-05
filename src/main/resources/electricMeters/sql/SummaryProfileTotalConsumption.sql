609515a61c7ec358cce58a373315a92ed407d4d9
a.isachenkov <a.isachenkov@itlc.ru>
2024-04-05 18:31:03 +0800

    .

--- src/main/resources/electricMeters/sql/SummaryProfileTotalConsumption.sql
+++ src/main/resources/electricMeters/sql/SummaryProfileTotalConsumption.sql
@@ -1,5 +1,5 @@
 -- Итоговое потребление на месяц
 select sum(aPos) as VALUE
 from PROFILE_STRS_WITH_DEDUCTION
-where cast(strftime('%m', DATE) as Integer) = ? -- месяц
-  and cast(strftime('%Y', DATE) as Integer) = ? -- год
+where YEAR  = ? -- Год
+  and MONTH = ? -- Месяц
